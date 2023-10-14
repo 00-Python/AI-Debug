d149194e6b39c02b577507e14856c8f82cf8bd29
Joe <rwc.webster@gmail.com>
2023-10-14 05:27:07 +0100

    refactor

--- README.md
+++ README.md
@@ -1,108 +1,87 @@
-# AI Code Debugger
+# Python AI Code Debugger
 
-This script utilizes OpenAI's models to assist in debugging code by providing suggestions for identifying and fixing errors in your code.
+Welcome! This Python project offers you a remarkable debugging assistant powered by OpenAI's advanced machine learning models.
 
-## TODO
-1. Add the abillity to choose specific files from dir or exclude specific files
-1. Map the file structure to make the AI aware of it 
+## :star2: Features
 
-## Installation
+**Suggests Bug Fixes:** The AI suggests potential bug fixes for your code, so you can simply focus on development.
 
-1. Clone the repository to your local machine:
+**Token Count and Costs:** The application helps you estimate the tokens used in sending requests to the OpenAI API and provides a cost estimation as well!
 
+**Cache Facilities:** Not want to waste tokens on repeated issues? The application can save suggestions for earlier seen issues in a cache file.
+
+## :fast_forward: Quick Start
+
+1. **Get the files**
+
+   Clone the repo with this command:
    ```bash
-   git clone https://github.com/00-Python/AI-Debug.git 
+   git clone https://github.com/Your_User_Name/AI-Code-Debugger.git
    ```
 
-2. Navigate to the project directory:
+2. **Change directory**
 
+   Change to the `AI-Code-Debugger` directory:
    ```bash
-   cd AI-Debug
+   cd AI-Code-Debugger
    ```
 
-3. Install the required dependencies using pip:
+3. **Setup**
+
+   Install required dependencies using pip:
 
    ```bash
-   pip install openai tiktoken
+   pip install -r requirements.txt
    ```
 
-4. Set up your OpenAI API key as an environment variable:
+   Next, export the OpenAI API key as an environment variable:
 
    ```bash
-   export OPENAI_API_KEY=your-api-key
+   export OPENAI_API_KEY='your_api_key'
    ```
 
-## Usage
-Check the help menu:
+4. **Run**
+
+   Now, you're ready for debugging:
+
+   ```bash
+   python debug.py -l Python -p /path/to/your/code -e "Error message here"
+   ```
+
+## :whale: Detailed Usage
+
+The Python AI Code Debugger is designed to be highly flexible and customizable to accommodate various debugging needs.
+
+You can launch the Python Debugger with various options to control its behavior:
 
 ```bash
-python debug.py -h
+python debug.py -m gpt-3.5-turbo -t 1.0 -o 500 -l Python -p your_code_dir -e "Your error message"
 ```
 
-Output:
-```
-usage: debug.py [-h] [-m MODEL] [-t TEMPERATURE] [-o MAX_OUTPUT_TOKENS] [-l LANGUAGE] [-p PATH] [-e ERROR] [--clear-cache]
+Where:
+- `-m, --model`: OpenAI Model to use for debugging assistance. Defaults to `gpt-3.5-turbo`. (supported: 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k')
+- `-t, --temperature`: The creative force of the prediction. Increase for more diverse output. Default is `1` and acceptable range is `0 - 2`.
+- `-o, --max-output-tokens`: The maximum tokens that the model will use to produce the output. Default is model specific.
+- `-l, --language`: The programming language of the debugging code. Currently supported: Python, Javascript, Java, C/C++, C#.
+- `-p, --path`: Path to the directory or file containing the code.
+- `-e, --error`: Error message or a description of the issue.
 
-AI Code Debugger & Helper CLI
+**Note:** You can always use the `-h` or `--help` argument to see all available options for the Python Code Debugger.
 
-options:
-  -h, --help            show this help message and exit
-  -m MODEL, --model MODEL
-                        Name of the OpenAI model to use (default: 'gpt-3.5-turbo-16k' Others: 'gpt-4', 'gpt-3.5-turbo')
-  -t TEMPERATURE, --temperature TEMPERATURE
-                        Temperature of response (Default is 1, can be between 0 and 2)
-  -o MAX_OUTPUT_TOKENS, --max-output-tokens MAX_OUTPUT_TOKENS
-                        Maximum output tokens in response (Default: GPT-3.5 Turbo: 4096 tokens, GPT-3.5-Turbo 16k: 16,384 tokens, GPT-4: 8192 tokens, GPT-4 32K: 32768 tokens)
-  -l LANGUAGE, --language LANGUAGE
-                        Programming language of the code
-  -p PATH, --path PATH  Path to the directory containing code files
-  -e ERROR, --error ERROR
-                        Specific error message or description of the error or issue
-  --clear-cache         Clear the cache
-```
+## :ticket: Need Help?
 
-Run the script using the following command:
+If you're stuck at any point, be sure to post the issue to the repository's Issue Tracker. We're here to help.
 
-```bash
-python debug.py [--model <openai_model>] [--temperature <model_temperature>] [--max-output-tokens <max_output_tokens>] [--language <programming_language>] --path <path_to_code_directory or file> --error "<error_description>"
-```
+Or better yet, contribute and open a pull request with your fixes for the community! We're always looking out for folks who want to help out.
 
-Replace the placeholders with appropriate values:
+## :heart: Contributing
 
-- `<openai_model>`: (optional) The OpenAI model to use. Default: 'gpt-3.5-turbo-16k'. Options: 'gpt-4', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo'.
-- `<model_temperature>`: (optional) Temperature of response. Default: 1.
-- `<max_output_tokens>`: (optional) Maximum output tokens in response.
-- `<programming_language>`: (optional) The programming language of the code you want to debug.
-- `<path_to_code_directory or file>`: The path to the directory containing your code files or path to the single file.
-- `<error_description>`: A description of the error or issue you are facing or the Traceback
+We'd love to welcome contributors who can help make this project better. If you're interested, do check out the [Contributing Guide](CONTRIBUTING.md) and [Code of Conduct](CODE_OF_CONDUCT.md).
 
-Example:
+## :bookmark: License
 
-```bash
-python debug.py --model gpt-3.5-turbo --language Python --path /path/to/your/code/directory/or/file --error "Code is throwing an IndexError."
-```
+This project is licensed under the GNU General Public License v3.0. See the [LICENSE](LICENSE.md) file for more information.
 
-The script will analyze the code files in the specified directory or a single file and provide AI-powered suggestions to debug the issue. 
+## :clap: Join the Community
 
-If for any reasons, you wanted to clear the previously cached error suggestions, you can do it by using `--clear-cache`:
-
-```bash
-python debug.py --clear-cache
-```
-This will clear all previously cached suggestions.
-
-## Detailed Explanation
-
-Before generating a debugging suggestion for your code, the script first counts the number of tokens that would be used up with the current request. The number of tokens is directly related to how many bytes of data you're sending and receiving from the API.
-
-The OpenAI API charges you based on these tokens.
-
-After the token count, the script estimates the cost of using the API for the current debugging task. This estimation accounts for the cost of both input tokens (tokens used to send your code and request to the API) and output tokens if output tokens limit is specified by the user (tokens used by the API to generate the response).
-
-You will be presented the cost estimation before the request is sent to the API for processing. This gives you the opportunity to review and minimize charges where necessary.
-
-If the same error occurs in the same code, the script handles this efficiently by using a caching system to store previously generated debugging suggestions. If a cache of the debugging suggestion for the current code and error exists, the script fetches it from the cache instead of sending another request to the API.
-
-## License
-
-This project is licensed under the GNU v3.0 - see the [LICENSE](LICENSE) file for details.
+Don't forget to star the repo if you like what you see, and watch for new updates.
--- debug.py
+++ debug.py
@@ -1,62 +1,226 @@
-import argparse
 import os
 import sys
-from system import CodeDebugger
+import openai
+import pickle
+import hashlib
+import tempfile
+from platform import system as platform_system
+import tiktoken
+import argparse
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="AI Code Debugger & Helper CLI")
+
+class CodeDebugger:
+    def __init__(self, model, model_temperature, max_output_tokens):
+        self.openai_model = model
+        self.model_temperature = model_temperature
+        # self.max_output_tokens = {"gpt-3.5-turbo": 4096, "gpt-4": 8192, "gpt-4-32k": 32768, "gpt-3.5-turbo-16k": 16384}.get(self.openai_model, None)
+        self.max_output_tokens = max_output_tokens
+        cache_file_name = 'cache.pickle'
+        self.tmp_cache_dir = os.path.join(
+            tempfile.gettempdir(), cache_file_name)
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+        self.messages = [
+            {"role": "system", "content": "You are a detailed code debugger assistant that doesn't miss a single error. You always point out where the error is and what is causing it."}
+        ]
+        self.encoder = tiktoken.encoding_for_model(self.openai_model)
+
+
+    def print_tokens_and_costs(self, code_dict):
+        code_content_combined = "".join(code_dict.values())
+        input_tokens = self.count_tokens(str(self.messages))
+        print(f"Total tokens in data: {input_tokens}")
+        input_cost, output_cost = self.calculate_cost(input_tokens)
+        print(f"Total cost: {self.format_cost(input_cost+output_cost, 4)} (Input tokens cost: {self.format_cost(input_cost, 9)}, Output tokens cost: {self.format_cost(output_cost, 5)})")
+
+    def get_files(self, path):
+        ignore_dirs = {"env", "venv", "__pycache__", ".git"}
+        files_dict = {}
+        file_and_dir_list = []
+
+        # Check if the path is a valid directory
+        if not os.path.isdir(path):
+            print("Invalid path provided.")
+            return files_dict
+
+        # Create a list of files and directories
+        for root, dirs, files in os.walk(path, topdown=True):
+            dirs[:] = [d for d in dirs if d not in ignore_dirs and not d.startswith('.')]
+            for file in files:
+                filepath = os.path.join(root, file)
+                file_and_dir_list.append(filepath)
+            for dir in dirs:
+                dirpath = os.path.join(root, dir)
+                file_and_dir_list.append(dirpath)
+
+        # Print out all available files and directories with paths relative to 'path'
+        for i, file_or_dir_path in enumerate(file_and_dir_list):
+            rel_path = os.path.relpath(file_or_dir_path, path)
+            if os.path.isdir(file_or_dir_path): # check if path is a directory
+                rel_path += "/" # append a '/' to the end of the path
+            print(f"{i+1}. {rel_path}")
+
+        file_choices = input(
+            "Choose files by typing the corresponding numbers (separated by a space for multiple files). You can also choose directories to select all files within that directory: "
+        ).split()
+        file_choices = [int(choice) for choice in file_choices]
+
+        # Check if the choices are valid
+        for choice in file_choices:
+            if 0 < choice <= len(file_and_dir_list):
+                file_or_dir_path = file_and_dir_list[choice - 1]
+                if os.path.isfile(file_or_dir_path):  # If it's a file
+                    with open(file_or_dir_path, "r") as file:
+                        rel_path = os.path.relpath(file_or_dir_path, path)
+                        files_dict[rel_path] = file.read()
+                else:  # If it's a directory
+                    for root, dirs, files in os.walk(file_or_dir_path):
+                        for file in files:
+                            filepath = os.path.join(root, file)
+                            rel_path = os.path.relpath(filepath, path)
+                            with open(filepath, "r") as file:
+                                files_dict[rel_path] = file.read()
+            else:
+                print(f"Invalid choice ({choice}). Please try again.")
+
+        return files_dict
+
+    # Token & Price Counter Methods
+    def count_tokens(self, data):
+        encoding = self.encoder.encode(data)
+        return len(encoding)
+
+    def calculate_cost(self, tokens):
+
+        # token price is per 1k tokens
+        prices = {
+            "gpt-4": {"input": 0.03, "output": 0.06},
+            "gpt-4-32K": {"input": 0.06, "output": 0.12},
+            "gpt-3.5-turbo": {"input": 0.0015, "output": 0.002},
+            "gpt-3.5-turbo-16k": {"input": 0.003, "output": 0.004},
+        }
+
+        for model_name, price_data in prices.items():            
+            if self.openai_model == model_name:
+                cost_input = (price_data["input"] * tokens) / 1000
+                cost_output = (price_data["output"] * self.max_output_tokens)  / 1000
+                return cost_input, cost_output
+
+        return None, None
+
+
+    # Convert cost to human-readable format
+    def format_cost(self, cost, decimals):
+        return f"£{cost:.{decimals}f}"
+
+    # Cache methods
+
+    def save_cache(self, cache_data):
+        with open(self.tmp_cache_dir, 'wb') as file:
+            pickle.dump(cache_data, file)
+
+    def load_cache(self):
+        if os.path.exists(self.tmp_cache_dir):
+            with open(self.tmp_cache_dir, 'rb') as file:
+                try:
+                    return pickle.load(file)
+                except (pickle.PickleError, EOFError):
+                    pass
+        return {}
+
+    def clear_cache(self):
+        if os.path.exists(self.tmp_cache_dir):
+            os.remove(self.tmp_cache_dir)
+            print("Cache cleared.")
+        else:
+            print("Cache is already empty.")
+
+    def hash_code(self, code_content):
+        hash_object = hashlib.md5(code_content.encode())
+        return hash_object.hexdigest()
+
+    def check_cache(self, model, messages, error, code_content):
+        cache_data = self.load_cache()
+
+        if error in cache_data:
+            cached_code_hash, cached_result = cache_data[error]
+            current_code_hash = self.hash_code(code_content)
+
+            if current_code_hash == cached_code_hash:
+                print("Using cached suggestions:")
+                print(cached_result)
+                return
+
+        computed_result = self.get_suggestions_from_openai(model, messages)
+
+        cache_data[error] = (self.hash_code(code_content), computed_result)
+        self.save_cache(cache_data)
+
+        print("Suggestions:")
+        print(computed_result)
+
+    # Debug Methods
+
+    def get_suggestions_from_openai(self, model, messages):
+        if self.max_output_tokens != 0:
+            response = openai.ChatCompletion.create(model=model,max_tokens=self.max_output_tokens, temperature=self.model_temperature, messages=messages)
+        else:
+            response = openai.ChatCompletion.create(model=model, temperature=self.model_temperature, messages=self.messages)
+
+        return response.choices[0].message["content"].strip()
+
+
+    def construct_messages(self, language, code_dict, error):
+        self.messages.append({"role": "user", "content": f"Debug the following {language} code:"})
+
+        for rel_filepath, code_content in code_dict.items():
+            self.messages.append({"role": "assistant", "content": f"File: {rel_filepath}\n\n{code_content}"})
+
+        self.messages.append({"role": "user", "content": f"Problem/Error: {error}"})
+
+        return self.messages
+
+def run_debugger():
+    # Parsing command line arguments
+    parser = argparse.ArgumentParser(description="AI Code Debugger & Helper CLI")
     parser.add_argument("-m", "--model", type=str, default="gpt-3.5-turbo-16k",
                         help="Name of the OpenAI model to use (default: 'gpt-3.5-turbo-16k' Others: 'gpt-4', 'gpt-3.5-turbo')")
     parser.add_argument("-t", "--temperature", type=float, default=1,
                         help="Temperature of response (Default is 1, can be between 0 and 2)")
     parser.add_argument("-o", "--max-output-tokens", type=int, default=0,
                         help="Maximum output tokens in response (Default: GPT-3.5 Turbo: 4096 tokens, GPT-3.5-Turbo 16k: 16,384 tokens, GPT-4: 8192 tokens, GPT-4 32K: 32768 tokens)")
-    parser.add_argument("-l", "--language", type=str,
-                        help="Programming language of the code")
-    parser.add_argument("-p", "--path", type=str,
-                        help="Path to the directory containing code files")
-    parser.add_argument("-e", "--error", type=str,
-                        help="Specific error message or description of the error or issue")
-    parser.add_argument(
-        "--clear-cache", action='store_true', help="Clear the cache")
+    parser.add_argument("-l", "--language", type=str, help="Programming language of the code")
+    parser.add_argument("-p", "--path", type=str, help="Path to the directory containing code files")
+    parser.add_argument("-e", "--error", type=str, help="Specific error message or description of the error or issue")
+    parser.add_argument("--clear-cache", action='store_true', help="Clear the cache")
     args = parser.parse_args()
 
-    debugger = CodeDebugger(str(args.model), args.temperature, int(args.max_output_tokens))    
-
+    # Creating an debugger instance
+    debugger = CodeDebugger(str(args.model), args.temperature, int(args.max_output_tokens))
 
     if args.clear_cache:
         debugger.clear_cache()
         return
 
+    # Getting code files
     code_dict = debugger.get_files(args.path)
-
 
     if not code_dict:
         print("No code files found.")
         return
 
+    # Combine code content and create hash
     code_content_combined = "".join(code_dict.values())
     code_hash = debugger.hash_code(code_content_combined)
-    cache_data = debugger.load_cache()
 
+    cache_data = debugger.load_cache()
     if code_hash in cache_data:
         cached_code_hash, cached_result = cache_data[code_hash]
-
         if cached_code_hash == code_hash:
             print("Using cached suggestions:")
             print(cached_result)
             return
 
-    debugger.messages.append(
-        {"role": "user", "content": f"Debug the following {args.language} code:"})
-
-    for rel_filepath, code_content in code_dict.items():
-        debugger.messages.append(
-            {"role": "assistant", "content": f"File: {rel_filepath}\n\n{code_content}"})
-
-    debugger.messages.append({"role": "user", "content": args.error})
-
+    constructed_messages = debugger.construct_messages(args.language, code_dict, args.error)
 
     input_tokens = debugger.count_tokens(str(debugger.messages))
     print(f"Total tokens in data: {input_tokens}")
@@ -64,12 +228,16 @@ def main():
     input_cost, output_cost = debugger.calculate_cost(input_tokens)
     print(f"Total cost: {debugger.format_cost(input_cost+output_cost, 4)} (Input tokens cost: {debugger.format_cost(input_cost, 9)}, Output tokens cost: {debugger.format_cost(output_cost, 5)})")
 
+    # Confirm and print result
     if input('Continue? (y/n) ').lower() == 'n':
         sys.exit()
     print()
-    
+
     debugger.check_cache(args.model, debugger.messages,
                         args.error, code_content_combined)
 
+
+
+
 if __name__ == "__main__":
-    main()
+    run_debugger()
