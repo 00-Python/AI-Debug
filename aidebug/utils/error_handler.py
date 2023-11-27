6cbe36a884cb128f43f2b1ccdbd3a24322c64f99
Joe <rwc.webster@gmail.com>
2023-11-27 20:44:26 +0000

    Handled common errors

--- aidebug/utils/error_handler.py
+++ aidebug/utils/error_handler.py
@@ -11,7 +11,7 @@ def error_handler(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            print(f'Error: {e}')
+            print(f'Error: {e}\nCheck the error.log file for more information...')
 
             current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             error_message = str(e)
@@ -20,6 +20,23 @@ def error_handler(func):
             logging.error(f'{current_time} - {error_message}\n{traceback_message}')
 
             # Additional cleanup or error handling code can be added here
+        except KeyboardInterrupt as e:
+            print('User Exited!')
+            raise
+        except FileNotFoundError as fnf_error:
+            print(f'File not found error: {fnf_error}')
+        except PermissionError as pe_error:
+            print(f'Permission error: {pe_error}')
+        except ValueError as value_error:
+            print(f'Value error: {value_error}')
+        except KeyError as key_error:
+            print(f'Key error: {key_error}')
+        except IndexError as index_error:
+            print(f'Index error: {index_error}')
+        except TypeError as type_error:
+            print(f'Type error: {type_error}')
+        except AttributeError as attribute_error:
+            print(f'Attribute error: {attribute_error}')
 
             return None
 
